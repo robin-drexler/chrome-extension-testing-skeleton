15c55dc7b906a53bbbf7438d378fcdbcd1b44569
Robin Drexler <drexler.robin@gmail.com>
2015-04-22 23:01:56 +0200

    some basic mocha setup up and running
    try `mocha test.js --timeout 5000`
    
    unfortunately, the driver setup is somewhat weird.
    driver needs to be function; directly writing this.driver causes
    the driver not to work in it() functions. I have no clue why. :/

--- tests/test.js
+++ tests/test.js
@@ -1,25 +1,54 @@
 var chromedriver = require('chromedriver');
-
-chromedriver.start();
-//run your tests
-
 var webdriver = require('selenium-webdriver'),
     chrome = require('selenium-webdriver/chrome'),
     By = require('selenium-webdriver').By,
     until = require('selenium-webdriver').until,
     chromeOptions = new chrome.Options;
 
+var expect = require('chai').expect;
+
+
 chromeOptions.addArguments("load-extension=../extension");
 
-var driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .usingServer('http://localhost:9515')
-    .setChromeOptions(chromeOptions)
-    .build();
 
-driver.get('http://www.google.com/ncr');
-driver.findElement(By.tagName('body')).getInnerHtml().then(function(d) {
-  console.log(d);
+describe('extension', function () {
+
+  before(function (done) {
+    chromedriver.start();
+
+    var driver = new webdriver.Builder()
+        .forBrowser('chrome')
+        .usingServer('http://localhost:9515')
+        .setChromeOptions(chromeOptions);
+
+    var builtDriver;
+
+    // I have no clue, why calling `build` right here causes the driver not to work later on
+    // doing it on demand in a function seems to work
+    // race condition?
+    this.driver = function() {
+      if (!builtDriver) {
+        builtDriver = driver.build();
+      }
+      return builtDriver;
+    };
+
+    done();
+  });
+
+  after(function (done) {
+    this.driver().quit().then(function () {
+      chromedriver.stop();
+      done();
+    });
+  });
+
+  it('displays TEST on any page', function (done) {
+    this.driver().get('http://www.google.com/ncr');
+
+    this.driver().findElement(By.tagName('body')).getInnerHtml().then(function (d) {
+      expect(d).to.contain('TEST');
+      done();
+    });
+  });
 });
-driver.quit();
-chromedriver.stop();
